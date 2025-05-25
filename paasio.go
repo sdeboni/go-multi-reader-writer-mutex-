c11d8c3fe8d3cbe05142e5099089a0c95b48442a
Sam De Boni <sdeboni@gmail.com>
2025-05-25 08:24:18 -0400

    adds synchronization using RWMutex. Tests passing

--- paasio.go
+++ paasio.go
@@ -1,16 +1,21 @@
 package paasio
 
-import "io"
+import (
+  "io"
+  "sync"
+)
 
 type readCounter struct {
   reader io.Reader
   bytesRead int64
   callCount int
+  mu sync.RWMutex
 }
 type writeCounter struct {
   writer io.Writer
   bytesWritten int64
   callCount int
+  mu sync.RWMutex
 }
 
 type readWriteCounter struct {
@@ -39,23 +44,37 @@ func NewReadWriteCounter(readwriter io.ReadWriter) ReadWriteCounter {
 
 func (rc *readCounter) Read(p []byte) (int, error) {
   bytesRead, err := rc.reader.Read(p)
+
+  rc.mu.Lock()
+  defer rc.mu.Unlock()
+
   rc.callCount++
   rc.bytesRead += int64(bytesRead)
   return bytesRead, err
 }
 
 func (rc *readCounter) ReadCount() (int64, int) {
+  rc.mu.RLock()
+  defer rc.mu.RUnlock()
+
   return rc.bytesRead, rc.callCount
 }
 
 func (wc *writeCounter) Write(p []byte) (int, error) {
   bytesWritten, err := wc.writer.Write(p)
+
+  wc.mu.Lock()
+  defer wc.mu.Unlock()
+
   wc.callCount++
   wc.bytesWritten += int64(bytesWritten)
   return bytesWritten, err
 }
 
 func (wc *writeCounter) WriteCount() (int64, int) {
+  wc.mu.RLock()
+  wc.mu.RUnlock()
+
   return wc.bytesWritten, wc.callCount
 }
 
